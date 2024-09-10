596da3d1b19334bcea9268d1da883162f8293fb3
Avana Vana <dear.avana@gmail.com>
2024-09-09 23:52:37 -0400

    chore(figma.manifest.ts): update figma manifest

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,6 +1,6 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
-  name: 'Repeat Grid',
+  name: 'Figma Plugin Template (React/Vite/Tailwind)',
   id: '1407643997043710374',
   api: '1.0.0',
   main: 'plugin.js',
@@ -8,5 +8,4 @@ export default {
   capabilities: [],
   enableProposedApi: false,
   editorType: [ 'figma' ],
-  relaunchButtons: [{ command: 'edit', name: 'Edit grid' }]
 };
